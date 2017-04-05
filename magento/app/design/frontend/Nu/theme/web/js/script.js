4bd7c0bfaf159418f3a00271e2ee2ed22c789437
Gandhi Selvaraj <gandhi.selvaraj@nutechnologyinc.com>
2017-04-05 18:38:43 +0530

    UI Home page Update
    Gandhi Selvaraj

--- magento/app/design/frontend/Nu/theme/web/js/script.js
+++ magento/app/design/frontend/Nu/theme/web/js/script.js
@@ -2,27 +2,28 @@ var j = jQuery.noConflict();
 
 j( document ).ready(function() {
    // Banner 
-   var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper('.banner', {
         pagination: '.swiper-pagination',
-		nextButton: '.swiper-button-next',
+        nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         slidesPerView: 1,
         paginationClickable: true,
-        spaceBetween: 30,
-		autoplay: 2500,
+        spaceBetween: 10,
+		autoplay: false,
+        effect:'fade',
         autoplayDisableOnInteraction: true,
 		breakpoints: {
             1024: {
                 slidesPerView: 1,
-                spaceBetween: 40
+                spaceBetween: 10
             },
             768: {
                 slidesPerView: 1,
-                spaceBetween: 30
+                spaceBetween: 10
             },
             640: {
                 slidesPerView: 1,
-                spaceBetween: 20
+                spaceBetween: 10
             },
             320: {
                 slidesPerView: 1,
@@ -31,34 +32,31 @@ j( document ).ready(function() {
         }
     });
 		
-	j(".swiper-container").hover(function(){
+	j(".banner").hover(function(){
 		swiper.stopAutoplay();
 	}, function(){
 		swiper.startAutoplay();
 	});
 
     // Brand show 
-    var swiper = new Swiper('.swiper-container-brand', {
-        pagination: '.swiper-pagination',
-		nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 6,
+    var swiper = new Swiper('.brand', {
+        slidesPerView: 8,
         paginationClickable: true,
-        spaceBetween: 30,
+        spaceBetween: 10,
 		autoplay: 2500,
         autoplayDisableOnInteraction: true,
 		breakpoints: {
             1024: {
-                slidesPerView: 4,
-                spaceBetween: 40
+                slidesPerView: 8,
+                spaceBetween: 10
             },
             768: {
                 slidesPerView: 4,
-                spaceBetween: 30
+                spaceBetween: 10
             },
             640: {
                 slidesPerView: 2,
-                spaceBetween: 20
+                spaceBetween: 10
             },
             320: {
                 slidesPerView: 1,
@@ -67,9 +65,96 @@ j( document ).ready(function() {
         }
     });
 		
-	j(".swiper-container-brand").hover(function(){
+	j(".brand").hover(function(){
 		swiper.stopAutoplay();
 	}, function(){
 		swiper.startAutoplay();
 	});
+
+    // Catlog 
+    var swiper = new Swiper('.catlog', {
+		nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 10,
+		autoplay: false,
+        autoplayDisableOnInteraction: true,
+		breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 10
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
+
+    // zapatos
+    var swiper = new Swiper('.zapatos', {
+		nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 10,
+		autoplay: false,
+        autoplayDisableOnInteraction: true,
+		breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 10
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
+    
+    //estilos 
+    var swiper = new Swiper('.estilos', {
+		nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 10,
+		autoplay: false,
+        autoplayDisableOnInteraction: true,
+		breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 10
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
 }); 
